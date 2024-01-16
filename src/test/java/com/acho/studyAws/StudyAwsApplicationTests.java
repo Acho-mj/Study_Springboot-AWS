1290f6f4179140bec3e6aa4c69db0180b712d3df
Choi MinJung <mj020503@naver.com>
2024-01-16 22:12:02 +0900

    기본 환경설정

---
+++ src/test/java/com/acho/studyAws/StudyAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acho.studyAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
