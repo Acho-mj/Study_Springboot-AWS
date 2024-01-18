877369185b2ed0ed86c0d4b4818d02a206175ca7
Choi MinJung <mj020503@naver.com>
2024-01-18 23:10:37 +0900

    feat: HelloController 및 테스트 코드 작성

--- src/main/java/com/acho/studyAws/StudyAwsApplication.java
+++ src/main/java/com/acho/studyAws/StudyAwsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyAwsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudyAwsApplication.class, args);
 	}
-
 }
---
+++ src/test/java/com/acho/studyAws/dto/HelloResponseDto.java
@@ -0,0 +1,4 @@
+package com.acho.studyAws.dto;
+
+public class HelloResponseDto {
+}
