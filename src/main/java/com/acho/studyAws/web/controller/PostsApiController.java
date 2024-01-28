c534619cfba610b4b79d143ec0220b699451aea1
Choi MinJung <mj020503@naver.com>
2024-01-28 22:45:59 +0900

    feat: 등록, 수정, 조회 API 만들기

---
+++ src/main/java/com/acho/studyAws/web/controller/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.acho.studyAws.web.controller;
+
+import com.acho.studyAws.service.posts.PostsService;
+import com.acho.studyAws.web.dto.PostsResponseDto;
+import com.acho.studyAws.web.dto.PostsSaveRequestDto;
+import com.acho.studyAws.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    // 등록
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    // 수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    // 조회
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
+
