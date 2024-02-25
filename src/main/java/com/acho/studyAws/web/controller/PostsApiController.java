322037c903156e7fb9b6d6f2854626e82d9982b7
Choi MinJung <mj020503@naver.com>
2024-02-25 13:26:24 +0900

    feat: 글 삭제 기능 추가

--- src/main/java/com/acho/studyAws/web/controller/PostsApiController.java
+++ src/main/java/com/acho/studyAws/web/controller/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.acho.studyAws.web.controller;
 
 import com.acho.studyAws.service.posts.PostsService;
+import com.acho.studyAws.web.dto.PostsListResponseDto;
 import com.acho.studyAws.web.dto.PostsResponseDto;
 import com.acho.studyAws.web.dto.PostsSaveRequestDto;
 import com.acho.studyAws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,6 +31,20 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+
+
+    // 삭제
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
 
